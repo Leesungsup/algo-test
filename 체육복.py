cd0a098546f7ec52a8facb0c9a8e41e7dcbeedac
Leesungsup <tmdtjq0116@naver.com>
2021-10-14 22:08:15 +0900

    set을 활용하여 풀어야한다. 중복을 없애고 차집합을 구할 수 있기 때문이다.

---
+++ 체육복.py
@@ -0,0 +1,12 @@
+def solution(n,reserve,lost):
+    answer=0
+    reserve_set=set(reserve)-set(lost)
+    lost_set=set(lost)-set(reserve)
+    for i in reserve_set:
+        if i-1 in lost_set:
+            lost_set.remove(i-1)
+        elif i+1 in lost_set:
+            lost_set.remove(i+1)
+    answer=n-len(lost_set)
+    print(answer)
+    return answer
