81da6ada6b4212b4a80e1fa29349400e5dc8a135
Leesungsup <tmdtjq0116@naver.com>
2021-10-25 20:17:34 +0900

    bfs 활용

---
+++ 네트워크.py
@@ -0,0 +1,16 @@
+from collections import deque
+def solution(n,computers):
+    answer=0
+    q=[]
+    q=deque()
+    visited=[0]*(n)
+    while 0 in visited:
+        q.append(visited.index(0))
+        while q:
+            v=q.popleft()
+            visited[v]=1
+            for i in range(n):
+                if visited[i]==0 and computers[v][i]==1:
+                    q.append(i)
+        answer+=1
+    print(answer)
