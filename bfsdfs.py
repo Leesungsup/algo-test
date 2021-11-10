c8c8ecebdc0e3e629259d76c1e319731a9fd4fbd
Leesungsup <tmdtjq0116@naver.com>
2021-11-10 23:57:50 +0900

    연습

--- bfsdfs.py
+++ bfsdfs.py
@@ -18,3 +18,44 @@ def solution(numbers,target):
 numbers=list(map(int,input().split()))
 target=int(input())
 solution(numbers,target)
+def solution1(numbers,target):
+    answer=0
+    q=[]
+    n=len(numbers)
+    q=deque()
+    q.append((numbers[0],0))
+    q.append((-numbers[0],0))
+    while q:
+        num,i=q.popleft()
+        if i+1==n:
+            if num==target:
+                answer+=1
+        else:
+            q.append((num+numbers[i+1],i+1))
+            q.append((num-numbers[i+1],i+1))
+    print(answer)
+    return answer
+def dfs(i,n,visited,computers):
+    visited[i]=1
+    for connect in range(n):
+        if i!= connect and computers[i][connect]==1:
+            if visited[connect]==0:
+                dfs(connect,n,visited,computers)
+def solution2(n,computers):
+    answer=0
+    visited=[0]*(n)
+    for i in range(n):
+        if visited[i]==0:
+            dfs(i,n,visited,computers)
+            answer+=1
+    return answer
+n,m=map(int,input().split())
+graph=[[] for _ in range(n+1)]
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+def dfs(x,y):
+    q=deque((x,y))
+    while q:
+        x,y=q.popleft()
--- 다익스트라.py
+++ 다익스트라.py
@@ -22,3 +22,25 @@ def dijkstra(start):
                 heapq.heappush(q,(dist+i[1],i[0]))
 dijkstra(start)
 print(distance)
+import heapq
+import sys
+INF=int(1e9)
+n,m=map(int,input().split())
+graph=[[] for _ in range(n+1)]
+distance = [INF]*(n+1)
+for i in range(m):
+    a,b,c=map(int,sys.stdin.readline().split())
+    graph[a].append((b,c))
+start=int(input())
+def dijkstra(start):
+    distance[start]=0
+    q=[]
+    heapq.heappush(q,(0,start))
+    while q:
+        dist,now=heapq.heappop(q)
+        if dist>distance[now]:
+            continue
+        for i in graph[now]:
+            if distance[i[1]]<dist+i[0]:
+                distance[i[1]]=dist+i[0]
+                heapq.heappush(q,(dist+i[0],i[1]))
