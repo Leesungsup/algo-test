391e80f5846b9b7f9f566a164bbb2f1a4071074a
Leesungsup <tmdtjq0116@naver.com>
2021-11-13 23:13:18 +0900

    순위 플로이드워셜 알고리즘

--- 순위.py
+++ 순위.py
@@ -23,3 +23,23 @@ def solution(n,results):
 n=5
 results=[[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]
 solution(n,results)
+def solution(n,results):
+    answer=0
+    graph=[[0]*(n+1) for _ in range(n+1)]
+    for i in results:
+        graph[i[0]][i[1]]=1
+        graph[i[1]][i[0]]=-1
+    for k in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if graph[i][j]==0:
+                    if graph[i][k]==1 and graph[k][j]==1:
+                        graph[i][j]=1
+                    if graph[i][k]==-1 and graph[k][j]==-1:
+                        graph[i][j]=-1
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if graph[i][j]==0:
+                answer+=1
+    print(graph)
+    return answer
