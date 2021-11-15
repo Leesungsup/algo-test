ef6ff265e7684d85ddf14d1702fbfeb7ba04a50a
Leesungsup <tmdtjq0116@naver.com>
2021-11-15 23:58:05 +0900

    트리를 만드는 줄 알았는데 아니었습니다. 그 상태로 배열을 이용해 해결하는 문제입니다.

--- n으로표현.py
+++ n으로표현.py
@@ -2,10 +2,13 @@ def solution(N,numbers):
     answer=0
     dp=[[]]
     for i in range(1,9):
+        print(i)
         temp=[]
         for j in range(1,i):
-            for op1 in dp[i]:
+            for op1 in dp[j]:
+                print(dp[j])
                 for op2 in dp[i-j]:
+                    print(dp[i-j])
                     temp.append(op1+op2)
                     if op1-op2>0:
                         temp.append(op1-op2)
