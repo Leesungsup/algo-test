8a810733a179cb55c2cf1cbaf16289343bd66a7a
Leesungsup <tmdtjq0116@naver.com>
2021-11-30 23:59:17 +0900

    동적

--- dynamic.py
+++ dynamic.py
@@ -1,4 +1,25 @@
 from collections import deque
+def solution2(N, number):
+    if N == number:
+        return 1
+    s = [ set() for x in range(8) ]
+    for i,x in enumerate(s, start=1):
+        x.add( int( str(N) * i ) )
+    for i in range(1, 8):
+        for j in range(i):
+            for op1 in s[j]:
+                for op2 in s[i-j-1]:
+                    s[i].add(op1 + op2)
+                    s[i].add(op1 - op2)
+                    s[i].add(op1 * op2)
+                    if op2 != 0:
+                        s[i].add(op1 // op2)
+        if number in s[i]:
+            answer = i + 1
+            break
+    else:
+        answer = -1
+    return answer
 def solution(m,n,puddles):
     answer=0
     puddles = [[q,p] for [p,q] in puddles]
