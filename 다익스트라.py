7b5a8ac00e3c7a73dfaaeb10639e81c8454980a7
Leesungsup <tmdtjq0116@naver.com>
2021-12-11 23:59:32 +0900

    다익스트라

--- 다익스트라.py
+++ 다익스트라.py
@@ -44,3 +44,58 @@ def dijkstra(start):
             if distance[i[1]]<dist+i[0]:
                 distance[i[1]]=dist+i[0]
                 heapq.heappush(q,(dist+i[0],i[1]))
+import heapq
+def BackTracking(maze,ex,ey):
+    dx=[-1,1,0,0]
+    dy=[0,0,-1,1]
+    foots=[]
+    foots.append([ex,ey])
+    foot = maze[ex][ey]
+    while(True):
+        for di in range(0,4):
+            x = ex + dx[di]
+            y = ey + dy[di]
+            if(maze[x][y] == foot-1):
+                foots.append([x,y])
+                ex = x
+                ey = y
+                foot = maze[x][y]
+                break
+        if(foot == 1):
+            foots.reverse()
+            return foots
+def dijkstra(x,y,ex,ey):
+    q=[]
+    x1=x
+    y1=y
+    dx=[-1,1,0,0]
+    dy=[0,0,-1,1]
+    map1 = [[-1] * len(map[n-1]) for _ in range(n)]
+    map1[x][y]=0
+    heapq.heappush(q,(1,(x,y)))
+    list=[]
+    list=deque()
+    while True:
+        dist,(x,y)=heapq.heappop(q)
+        list.append((x,y))
+        if map[x][y]==3:
+            break
+        for i in range(4):
+            xx=x+dx[i]
+            yy=y+dy[i]
+            if 0<=xx< n and 0<= yy < len(map[n-1]) and map1[xx][yy] < 0:
+                if map[xx][yy]==0 or map[xx][yy]==3:
+                    map1[xx][yy]=dist
+                    canvas.create_oval(yy * 30, xx * 30, yy * 30 + 30, xx * 30 + 30, fill="green")
+                    root.update()
+                    time.sleep(0.2)
+                    heapq.heappush(q,(dist+1,(xx,yy)))
+    route=BackTracking(map1,ex,ey)
+    route.append((ex,ey))
+    for i in route:
+        canvas.create_oval(i[1] * 30, i[0] * 30, i[1] * 30 + 30, i[0] * 30 + 30, fill="red")
+        root.update()
+        time.sleep(0.2)
+    messagebox.showinfo(title="성공", message="미로 찾기에 성공하셨습니다")
+    canvas.delete("all")
+    return
