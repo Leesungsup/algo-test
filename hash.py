0fcf012cbf4c87bed6e491e853c1544024912f53
Leesungsup <tmdtjq0116@naver.com>
2021-12-27 15:33:13 +0900

    hash

--- hash.py
+++ hash.py
@@ -1,4 +1,60 @@
 from collections import defaultdict
+def solution4(genres,plays):
+    answer=[]
+    genre_dic=defaultdict(lambda :0)
+    total_list=defaultdict(lambda:[])
+    for i in range(len(genres)):
+        genre_dic[genres[i]]+=plays[i]
+        total_list[genres[i]].append((plays[i],i))
+    genre_dic=sorted(genre_dic.items(),key=lambda x:x[1],reverse=True)
+    for i in total_list:
+        total_list[i]=sorted(total_list[i],key=lambda x:x[0],reverse=True)[:2]
+    while len(genre_dic)>0:
+        genre_max=genre_dic.pop(0)
+        for t in total_list:
+            if t==genre_max[0]:
+                if len(total_list[t])>1:
+                    answer.append(total_list[t][0][1])
+                    answer.append(total_list[t][1][1])
+                else:
+                    answer.append(total_list[t][0][1])
+    return answer
+def solution3(clothes):
+    answer=1
+    d=dict()
+    for i in clothes:
+        if i[1] not in d:
+            d[i[1]]=1
+        if i[1] in d:
+            d[i[1]]+=1
+    for i in d:
+        answer=answer*(d[i]+1)
+    answer-=1
+    return answer
+def solution2(phone_book):
+    answer=True
+    phone_book.sort()
+    for i in range(1,len(phone_book)):
+        if phone_book[i].find(phone_book[i-1])==0:
+            answer=False
+            return answer
+    return answer
+def solution1(participant,completion):
+    answer=''
+    p=dict()
+    for i in participant:
+        if i not in p:
+            p[i]=1
+        if i in p:
+            p[i]+=1
+    for i in completion:
+        if i in p:
+            p[i]-=1
+    for i in p:
+        if p[i]!=0:
+            answer=i
+    print(p)
+    return answer
 def solution(participant,completion):
     answer=''
     p=dict()
@@ -23,7 +79,7 @@ def solution1(phone_book):
             return answer
     return answer
 def solution2(clothes):
-    answer=0
+    answer=1
     q=dict()
     for i in clothes:
         if i[1] not in q:
@@ -31,7 +87,7 @@ def solution2(clothes):
         else:
             q[i[1]]+=1
     for i in q:
-        answer=answer*(d[i]+1)
+        answer=answer*(q[i]+1)
     answer-=1
     print(answer)
     return answer
