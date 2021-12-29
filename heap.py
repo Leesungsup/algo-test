a97efee99a911934b14f7099b72d6d8817b3261d
Leesungsup <tmdtjq0116@naver.com>
2021-12-29 17:07:55 +0900

    heap

--- heap.py
+++ heap.py
@@ -1,5 +1,73 @@
 import heapq
 INF=int(1e9)
+def solution6(jobs):
+    answer, now, i = 0, 0, 0
+    start = -1
+    heap = []
+
+    while i < len(jobs):
+        # 현재 시점에서 처리할 수 있는 작업을 heap에 저장
+        for j in jobs:
+            if start < j[0] <= now:
+                heapq.heappush(heap, [j[1], j[0]])
+
+        if len(heap) > 0: # 처리할 작업이 있는 경우
+            cur = heapq.heappop(heap)
+            start = now
+            now += cur[0]
+            answer += now - cur[1] # 작업 요청시간부터 종료시간까지의 시간 계산
+            i +=1
+        else: # 처리할 작업이 없는 경우 다음 시간을 넘어감
+            now += 1
+
+    return answer // len(jobs)
+def solution5(operations):
+    answer = []
+    q=[]
+    q1=[]
+    for i in operations:
+        s=i.split()
+        print(s)
+        if s[0]=='I':
+            heapq.heappush(q,int(s[1]))
+            heapq.heappush(q1,int(s[1])*-1)
+        else:
+            if len(q1)==0 or len(q)==0:
+                continue
+            elif s[1]=='1':
+                x=heapq.heappop(q1)
+                q.remove(x*-1)
+            else:
+                x=heapq.heappop(q)
+                q1.remove(x*-1)
+        print(q)
+    if len(q)==0 or len(q1==0):
+        answer=[0,0]
+    else:
+        x=heapq.heappop(q)
+        answer.append(x)
+        x=heapq.heappop(q1)
+        answer.append(x*-1)
+    return answer
+def solution4(scoville,K):
+    answer=0
+    heapq.heapify(scoville)
+    time=0
+    while scoville:
+        x=heapq.heappop(scoville)
+        if len(scoville)==0:
+            if x<K:
+                answer=-1
+                return answer
+        else:
+            if x<K:
+                x1=heapq.heappop()
+                x2=x+(x1*2)
+                heapq.heappush(scoville,x2)
+                answer+=1
+            else:
+                break
+    return answer
 def prim(graph,start,visited):
     mst=[]
     q=[]
