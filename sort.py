09e23c153bb68db903492222900e6f37d4e61a12
Leesungsup <tmdtjq0116@naver.com>
2021-12-30 12:27:23 +0900

    sort

--- sort.py
+++ sort.py
@@ -4,6 +4,22 @@ def quick(start,end,array):
     pivot=start
     left=start+1
     right=end
+    while left<right:
+        while array[pivot]>=array[left] and left<=end:
+            left+=1
+        while array[pivot]<=array[right] and right>start:
+            right-=1
+        if right>left:
+            array[right],array[left]=array[left],array[right]
+        else:
+            array[right],array[pivot]=array[pivot],array[right]
+
+def quick(start,end,array):
+    if start>=end:
+        return array
+    pivot=start
+    left=start+1
+    right=end
     while start<end:
         while array[left]<=array[pivot] and left<=end:
             left+=1
@@ -80,4 +96,32 @@ def solution1(numbers):
     array1=sorted(array,key=lambda x:x*3, reverse=True)
     answer=''.join(array1)
     print(answer)
+    return answer
+def solution5(array,commands):
+    answer=[]
+    for i in range(len(commands)):
+        array1=array[i[0]-1:i[1]]
+        array1.sort()
+        answer.append(array1[i[2]])
+    return answer
+def solutioin6(numbers):
+    answer=''
+    array=list(map(str,numbers))
+    numb=sorted(array,key=lambda x:x*3,reverse=True)
+    answer=str(int(''.join(numb)))
+    print(array)
+    return answer
+def solution7(citations):
+    answer=0
+    array=[]
+    array=sorted(citations)
+    h=len(array)
+    while True:
+        num=0
+        for i in array:
+            if h<=i:
+                num+=1
+            if num>=h:
+                return h
+        h=h-1
     return answer
