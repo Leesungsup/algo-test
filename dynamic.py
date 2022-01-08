dd39dbf8e443d2f7c8bd2c60d49e77d8ab5bbcf6
Leesungsup <tmdtjq0116@naver.com>
2022-01-08 17:09:46 +0900

    Dynamic

--- dynamic.py
+++ dynamic.py
@@ -1,4 +1,45 @@
 from collections import deque
+def solution4(triangle):
+    answer=0
+    n=len(triangle)
+    for i in range(1,n):
+        for j in range(i+1):
+            if j==0:
+                triangle[i][j]+=triangle[i-1][j]
+            elif i==j:
+                triangle[i][j]+=triangle[i-1][j-1]
+            else:
+                triangle[i][j]+=max(triangle[i-1][j-1],triangle[i-1][j])
+    ax=-987654321
+    for i in range(n):
+        if triangle[n-1][i]>ax:
+            ax=triangle[n-1][i]
+    answer=ax
+    print(answer)
+    return answer
+def solution3(N,number):
+    answer=0
+    if N==number:
+        return 1
+    s=[set() for x in range(8)]
+    for i,x in enumerate(s,start=1):
+        x.add(int(str(N)*i))
+    for i in range(1,8):
+        for j in range(i):
+            for op1 in s[j]:
+                for op2 in s[i-j-1]:
+                    s[i].add(op1+op2)
+                    s[i].add(op1-op2)
+                    s[i].add(op1*op2)
+                    if op2!=0:
+                        s[i].add(op1//op2)
+        if number in s[i]:
+            answer = i + 1
+            break
+    if answer==0:
+        answer=-1
+        return answer
+    return answer
 def solution2(N, number):
     if N == number:
         return 1
