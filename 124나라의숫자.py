de7b6f8747bd0dbcfec7c6d48a664f0f1eb642ec
Leesungsup <tmdtjq0116@naver.com>
2022-01-11 01:58:06 +0900

    124나라의 숫자

--- 124나라의숫자.py
+++ 124나라의숫자.py
@@ -1,3 +1,27 @@
+def solution2(n):
+    k=bin(n)
+    print(n)
+    save = []
+    while True:
+        a = int(n / 2)
+        b = int(n % 2)
+        save.insert(0, b)
+        if a != 0:
+            n = a
+        else:
+            break
+    final = ''.join(map(str, save))
+    print(final)
+def solution1(n):
+    answer=''
+    while n:
+        if n%3:
+            answer+=str(n%3)
+            n//=3
+        else:
+            answer+='4'
+            n=n//3-1
+    return answer[::-1]
 def solution(n):
     answer = ''
     while n:
