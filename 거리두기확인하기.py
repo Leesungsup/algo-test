5449efe9ed7e1b9444c472b0b79acdea3ce02403
Leesungsup <tmdtjq0116@naver.com>
2022-01-25 02:27:32 +0900

    거리두기 확인하기

--- 거리두기확인하기.py
+++ 거리두기확인하기.py
@@ -1,37 +1,43 @@
 from collections import deque
 def bfs(places,x,y):
-    visit=[[] for _ in range(len(places[0]))]
+    visit=[[0]*len(places) for _ in range(len(places))]
     dx=[-1,1,0,0]
     dy=[0,0,-1,1]
     q=deque()
-    q.append((x,y,1))
+    dist=0
+    q.append((x,y,dist))
     visit[x][y]=1
     while q:
         x,y,dist=q.popleft()
-        if places[nx][ny]=='P' and 0<d<3:
+        if places[x][y]=='P' and 0<dist<3:
             return False
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
             d=dist+1
-            if nx>=0 and nx<len(places) and ny>=0 and ny<len(places[0]):
-                if places[nx][ny]!='X' and visit[nx][ny]:
+            if nx>=0 and nx<len(places) and ny>=0 and ny<len(places):
+                if places[nx][ny]!='X' and visit[nx][ny]==0:
                     q.append((nx,ny,d))
                     visit[nx][ny]=1
     return True
 def solution(places):
     answer = []
+    number=0
     for i in places:
         chk=0
-        for j in range(len(i)):
-            for k in range(len(j[0])):
-                if places[i][j]=='P':
-                    if not bfs(places,i,j):
+        number+=1
+        for j in range(5):
+            for k in range(5):
+                if i[j][k]=='P':
+                    if not bfs(i,j,k):
+                        print(number)
                         answer.append(0)
                         chk=1
                         break
-                if chk==1:
-                    break
-            if chk==0:
-                answer.append(1)
+            if chk==1:
+                break
+        if chk==0:
+            answer.append(1)
+    print(answer)
     return answer
+solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]])
