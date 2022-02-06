13adcfbeca9383a7e297462f8855363269e9fe0b
Leesungsup <tmdtjq0116@naver.com>
2022-02-07 03:25:08 +0900

    다단계칫솔판매

---
+++ 다단계칫솔판매.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+def solution(enroll, referral, seller, amount):
+    answer = []
+    d=defaultdict()
+    k=defaultdict()
+    for i in enroll:
+        d[i]=''
+        k[i]=0
+    for i in range(len(enroll)):
+        for j in range(len(referral)):
+            if enroll[i]==referral[j]:
+                d[enroll[j]]=enroll[i]
+    #print(d)
+    #print(k)
+    for i in range(len(seller)):
+        c=seller[i]
+        k[c]+=(amount[i]*100)-((amount[i]*100)//10)
+        na=(amount[i]*100)//10
+        for j in range(len(enroll)):
+            if d[c]!='':
+                #print(c)
+                #print(na)
+                #print(d[c])
+                k[d[c]]+=na-na//10
+                na=na//10
+                c=d[c]
+                if d[c]=='':
+                    break
+            else:
+                break
+    #print(k)
+    for i in k:
+        answer.append(k[i])
+    #print(answer)
+    return answer
