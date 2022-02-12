4f99fb2300de1bd14293d2e79d8b5b9c9aeabd44
Leesungsup <tmdtjq0116@naver.com>
2022-02-13 01:06:07 +0900

    하노이탑

---
+++ 하노이탑.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer=[]
+    def move(n,start,to):
+        answer.append([start,to])
+    def hanoi(n,start,to,via):
+        if n==1:
+            move(1,start,to)
+        hanoi(n-1,start,via,to)
+        move(n,start,to)
+        hanoi(n-1,via,to,start)
+    hanoi(n,1,3,2)
