6ff2447eb935de1de1a92dfa09f6a45f330e00d5
Leesungsup <tmdtjq0116@naver.com>
2022-02-20 01:46:46 +0900

    스티커 모으기

---
+++ 스티커모으기.py
@@ -0,0 +1,14 @@
+def solution(sticker):
+    answer=0
+    n=len(sticker)
+    dp=[0]*n
+    dp[0]=sticker[0]
+    dp[1]=sticker[1]
+    for i in range(2,n-1):
+        dp[i]=max(dp[i-1],sticker[i]+dp[i-2])
+    dp2=[0]*n
+    dp2[0]=0
+    dp2[1]=sticker[1]
+    for i in range(2,n):
+        dp2[i]=max(dp2[i-1],sticker[i]+dp[i-2])
+    return max(max(dp),max(dp2))
