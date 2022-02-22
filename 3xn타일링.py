c657afb898c01a2c96137226318db44bc3dfc834
Leesungsup <tmdtjq0116@naver.com>
2022-02-23 03:12:38 +0900

    3xn 타일링

---
+++ 3xn타일링.py
@@ -0,0 +1,14 @@
+
+def solution(n):
+    if n%2==1:
+        return 0
+    dp=[0]*5000
+    dp[0]=1
+    dp[2]=3
+    for i in range(4,n+1,2):
+        dp[i]=dp[i-2]*3
+        for j in range(i-4,-1,-2):
+            dp[i]=dp[i]+dp[j]*2
+        dp[i]=dp[i]%1000000007
+    print(int(dp[n]))
+solution(4)
