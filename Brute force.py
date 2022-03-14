8c1e74e4d48beec4ca4e303c62dbb6e9b1c7e0de
Leesungsup <tmdtjq0116@naver.com>
2022-03-15 02:30:42 +0900

    KMP

--- Brute force.py
+++ Brute force.py
@@ -1,12 +1,26 @@
-target = 'ABC'
-text = 'ABABCA'
-
-for i in range(len(text)-len(target)):
+target='ABC'
+text='ABABCA'
+for i in  range(len(text)-len(target)):
     for j in range(len(target)):
-        if text[i+j] == target[j]:
+        if text[i+j]==target[j]:
             j+=1
         else:
             break
-
-    print('Identified')
+    print('Indentified')
     break
+def BruteForce(p,t):
+    i=0
+    j=0
+    while i<len(t) and j < len(p):
+        if t[i]!=p[j]:
+            i=i-j
+            j=-1
+        i+=1
+        j+=1
+    if j==len(p):
+        return i-len(p)
+    else:
+        return -1
+pattern="Python"
+text="Hello Python World"
+BruteForce(pattern,text)
