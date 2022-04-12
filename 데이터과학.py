a0006944a2a6768f30785dc80f41102c874d2031
Leesungsup <tmdtjq0116@naver.com>
2022-04-13 00:36:39 +0900

    데이터과학

--- 데이터과학.py
+++ 데이터과학.py
@@ -506,3 +506,276 @@ plt.title("Scatter Plot(female)", fontsize=15)
 plt.scatter(xf, yf, color='r')
 plt.scatter(xf1, yf1, color='b')
 plt.show()
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
+
+base_src='./drive/MyDrive'
+df=pd.read_excel(base_src+'/bmi_data_phw3.xlsx') #read csv file and store in df
+print(df)
+
+# Print dataset statistical data.
+print("Statistical data: ")
+print(df.describe())
+
+# Print Columns names.
+print("Columns names: ")
+print(df.columns.values)
+
+# Print Columns data types.
+print("Columns data types: ")
+print(df.dtypes)
+
+# Plot the height histogram (bins=10) for each BMI value.
+grid = sns.FacetGrid(df, col='BMI')
+grid.map(plt.hist, 'Height (Inches)', bins=10)
+plt.show()
+
+# Plot a weighted histogram (bins=10) for each BMI value.
+grid1 = sns.FacetGrid(df, col='BMI')
+grid1.map(plt.hist, 'Weight (Pounds)', bins=10)
+plt.show()
+
+# Convert Categories data to numbers.(sex(male, female) to 0, 1)
+LabelEncoder = LabelEncoder()
+LabelEncoder.fit(df['Sex'])
+df['Sex'] = LabelEncoder.transform(df['Sex'])
+
+# Normalize data frames to Standard Scaler
+standardScaler = preprocessing.StandardScaler()
+standardscaled_df = standardScaler.fit_transform(df)
+standardscaled_df = pd.DataFrame(standardscaled_df, columns=['Sex', 'Age', 'Height (Inches)', 'Weight (Pounds)', 'BMI'])
+
+# Normalize data frames to Min-Max Scaler
+minmaxScaler = preprocessing.MinMaxScaler()
+minmaxscaled_df = minmaxScaler.fit_transform(df)
+minmaxscaled_df = pd.DataFrame(minmaxscaled_df, columns=['Sex', 'Age', 'Height (Inches)', 'Weight (Pounds)', 'BMI'])
+
+# Normalize data frames to Robust Scaler
+robustScaler = preprocessing.RobustScaler()
+robustscaled_df = robustScaler.fit_transform(df)
+robustscaled_df = pd.DataFrame(robustscaled_df, columns=['Sex', 'Age', 'Height (Inches)', 'Weight (Pounds)', 'BMI'])
+
+#4 The plot output was previously scaled to height.
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(ncols=4, figsize=(10, 5))  # Create subplot for outputting 4 plots
+ax1.set_title('Before Scaling')
+sns.kdeplot(df['Height (Inches)'], ax=ax1)  # Plot scaling results for height
+
+ax2.set_title('After Standard Scaling')
+sns.kdeplot(standardscaled_df['Height (Inches)'], ax=ax2)  # Standard scaling results for height
+
+ax3.set_title('After MinMax Scaling')
+sns.kdeplot(minmaxscaled_df['Height (Inches)'], ax=ax3)  # Min-Max scaling results for height
+
+ax4.set_title('After Robust Scaling')
+sns.kdeplot(robustscaled_df['Height (Inches)'], ax=ax4)  # Robust scaling results for height
+
+plt.show()
+
+# 4 The plot output was previously scaled to weight
+fig, (ax5, ax6, ax7, ax8) = plt.subplots(ncols=4, figsize=(10, 5))  # Create subplot for outputting 4 plots
+ax5.set_title('Before Scaling')
+sns.kdeplot(df['Weight (Pounds)'], ax=ax5)  # Plot scaling results for weight
+
+ax6.set_title('After Standard Scaling')
+sns.kdeplot(standardscaled_df['Weight (Pounds)'], ax=ax6)  # Standard scaling results for weight
+
+ax7.set_title('After MinMax Scaling')
+sns.kdeplot(minmaxscaled_df['Weight (Pounds)'], ax=ax7)  # Min-Max scaling results for weight
+
+ax8.set_title('After Robust Scaling')
+sns.kdeplot(robustscaled_df['Weight (Pounds)'], ax=ax8)  # Robust scaling results for weight
+
+plt.show()
+
+# Parentheses in column names restrict coding. So change the name.
+df.rename(columns={'Height (Inches)': 'Height', 'Weight (Pounds)': 'Weight'}, inplace=True)
+
+# Assignment for Coordination
+x = df['Height']
+y = df['Weight']
+
+# Apply data to linear regression
+E = linear_model.LinearRegression()
+E.fit(x.values.reshape(-1, 1), y)  # Change the shape as it should be 2D.
+
+# Create new columns with predictions obtained by linear regression
+df['Weight(predict)'] = df.apply(lambda x: float(E.predict([[x.Height]])), axis=1)
+
+# Normalize the e values
+normalize = []
+e = df['Weight'] - df['Weight(predict)']
+# Calculate linear regression equations from predictions
+for value in e:
+    ze = (value - np.mean(e)) / np.std(e)
+    normalize.append(ze)
+
+# Plot a histogram showing the distribution of ze (~10 bins)
+plt.hist(normalize, bins=10)
+plt.title('Weight Reference Histogram')
+plt.xlabel('Ze')
+plt.ylabel('frequency')
+plt.show()
+
+#If it is less than -2, it is 0, if more than -2 and less than -1, it is 1, if it is more than -1 and less than 0, it is 2, if it is more than 0 and less than 2, it is 3, if it is 2 or more, it is 4
+# Determine the record with ze, set BMI
+bmi = []
+for i in normalize:
+    if i < -2:
+        bmi.append(0)
+    elif i>=-2 and i<-1:
+        bmi.append(1)
+    elif i>=-1 and i<0:
+        bmi.append(2)
+    elif i>=0 and i<2:
+        bmi.append(3)
+    elif i>=2:
+        bmi.append(4)
+df['BMI(predict)'] = bmi
+
+#Check how well the existing bmi matches the predicted bmi.
+print('Total number of bmi : ')
+print(len(df.values))
+count=0
+print('Corret Count : ')
+for i in df.values:
+    if i[4]==i[6]:
+        count+=1
+print(count)
+print(df[(df['BMI(predict)']==0)])
+print(df[(df['BMI(predict)']==4)])
+
+# For male dataset
+base_src='./drive/MyDrive'
+dfm = pd.read_excel(base_src+'/bmi_data_phw3.xlsx')  # Read the xlsx file and save it to the pandas data frame.
+
+# The parentheses in the column name restrict the coding. Therefore, change the name
+dfm.rename(columns={'Height (Inches)': 'Height', 'Weight (Pounds)': 'Weight'}, inplace=True)
+
+dfm1 = dfm.query("Sex=='Male'")  # Only import gender as male.
+dm = dfm1.copy()  # Because Setting With Copy Warning pops up, make a new copy and manage it.
+
+# Assignment for Coordination
+dm_x = dm['Height']
+dm_y = dm['Weight']
+
+# Apply data to linear regression
+dm_E = linear_model.LinearRegression()
+dm_E.fit(dm_x.values.reshape(-1, 1), dm_y)  # Change the shape as it should be 2D.
+
+# Create new columns with predictions obtained by linear regression
+dm['Weight(predict)'] = dfm1.apply(lambda x: float(dm_E.predict([[x.Height]])), axis=1)
+
+# Normalize the e values
+normalize_m = []
+dm_e = dm['Weight'] - dm['Weight(predict)']
+# Calculate the male's e by subtracting the original weight from the expected weight.
+for value in dm_e:
+    ze_m = (value - np.mean(dm_e)) / np.std(dm_e)
+    normalize_m.append(ze_m)
+
+# Plot a histogram showing the distribution of ze (~10 bins)
+plt.hist(normalize_m, bins=10)
+plt.title("Weight Reference Histogram(only male)")
+plt.xlabel('ze')
+plt.ylabel('frequency')
+plt.show()
+
+#If it is less than -2, it is 0, if more than -2 and less than -1, it is 1, if it is more than -1 and less than 0, it is 2, if it is more than 0 and less than 2, it is 3, if it is 2 or more, it is 4
+# Check records with ze, set BMI
+bmi_m = []
+for i in normalize_m:
+    if i < -2:
+        bmi_m.append(0)
+    elif i>=-2 and i<-1:
+        bmi_m.append(1)
+    elif i>=-1 and i<0:
+        bmi_m.append(2)
+    elif i>=0 and i<2:
+        bmi_m.append(3)
+    elif i>=2:
+        bmi_m.append(4)
+dm['BMI(predict)'] = bmi_m
+
+#Check how well the existing bmi matches the predicted bmi.
+print('Total number of male bmi : ')
+print(len(dm.values))
+count=0
+print('Correct Count : ')
+for i in dm.values:
+    if i[4]==i[6]:
+        count+=1
+print(count)
+print(dm[(dm['BMI(predict)']==0)])
+print(dm[(dm['BMI(predict)']==4)])
+
+# For female dataset
+base_src='./drive/MyDrive'
+dff = pd.read_excel(base_src+'/bmi_data_phw3.xlsx')  # Read the xlsx file and save it to a pandas dataframe.
+
+# Parentheses in column names restrict coding. So change the name.
+dff.rename(columns={'Height (Inches)': 'Height', 'Weight (Pounds)': 'Weight'}, inplace=True)
+
+dff1 = dff.query("Sex=='Female'")  # Only import gender as female.
+dfe = dff1.copy()  # A setting with a copy warning appears, so make a new copy and manage it.
+
+# Assignment for Coordination
+dfe_x = dfe['Height']
+dfe_y = dfe['Weight']
+
+# Apply data to linear regression
+dfe_E = linear_model.LinearRegression()
+dfe_E.fit(dfe_x.values.reshape(-1, 1), dfe_y)  # Change the shape as it should be 2D.
+
+dfe['Weight(predict)'] = dff1.apply(lambda x: float(dfe_E.predict([[x.Height]])), axis=1)  # Predict a woman's weight using the equation.
+
+dfe_e = dfe['Weight'] - dfe['Weight(predict)']
+# Calculate the woman's e by subtracting the original weight from the expected weight.
+
+normalize_f = []
+# Normalized Data
+for value in dfe_e:
+    ze_f = (value - np.mean(dfe_e)) / np.std(dfe_e)
+    normalize_f.append(ze_f)
+
+# Plot a histogram showing the distribution of ze (~10 bins).
+plt.hist(normalize_f, bins=10)
+plt.title("Weight Reference Histogram(only female)")
+plt.xlabel('ze')
+plt.ylabel('frequency')
+plt.show()
+
+#If it is less than -2, it is 0, if more than -2 and less than -1, it is 1, if it is more than -1 and less than 0, it is 2, if it is more than 0 and less than 2, it is 3, if it is 2 or more, it is 4
+# Check records with ze, set BMI
+bmi_f = []
+for i in normalize_f:
+    if i < -2:
+        bmi_f.append(0)
+        continue
+    elif i>=-2 and i<-1:
+        bmi_f.append(1)
+    elif i>=-1 and i<0:
+        bmi_f.append(2)
+    elif i>=0 and i<2:
+        bmi_f.append(3)
+    elif i>=2:
+        bmi_f.append(4)
+dfe['BMI(predict)'] = bmi_f
+
+#Check how well the existing bmi matches the predicted bmi.
+print('Total number of female bmi : ')
+print(len(dfe.values))
+count=0
+print('Correct Count : ')
+for i in dfe.values:
+    if i[4]==i[6]:
+        count+=1
+print(count)
+print(dfe[(dfe['BMI(predict)']==0)])
+print(dfe[(dfe['BMI(predict)']==4)])
