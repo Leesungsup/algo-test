86cb257b56acd22189d2343d52f9a894c9cef134
Leesungsup <tmdtjq0116@naver.com>
2022-04-19 02:00:58 +0900

    위상정렬

---
+++ 위상정렬.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+v,n=map(int,input().split())
+ma=[[] for _ in range(v+1)]
+indegree = [0] * (v + 1)
+print(ma)
+for i in range(n):
+    a,b=map(int,input().split())
+    ma[a].append(b)
+    indegree[b]+=1
+print(ma)
+q=deque()
+result=[]
+for i in range(1,v+1):
+    if indegree[i]==0:
+        q.append(i)
+while q:
+    start=q.popleft()
+    result.append(start)
+    for i in ma[start]:
+        indegree[i]-=1
+        if indegree[i]==0:
+            q.append(i)
+for i in range(len(result)):
+    print(result[i])
