3c1121b9bb4c814e5bae2c014d2f13c37f9a90e5
Leesungsup <tmdtjq0116@naver.com>
2022-09-08 01:46:07 +0900

    계산기

--- 계산기.py
+++ 계산기.py
@@ -1,3 +1,46 @@
+for test_case in range(10):
+    N = int(input())
+    s = input()
+    tokens = []
+    stack = []
+    result = []
+    for i in range(N):
+        if s[i].isdigit():
+            tokens.append(int(s[i]))
+        else:
+            if s[i] == ')':
+                chk = ''
+                while chk != '(':
+                    chk = stack.pop()
+                    tokens.append(chk)
+                tokens.pop()
+                continue
+            elif s[i] == '*':
+                while stack and stack[-1] == '*' and stack[-1] != "(":
+                    tokens.append(stack.pop())
+            elif s[i] == '+':
+                while stack and stack[-1] != "(":
+                    tokens.append(stack.pop())
+            stack.append(s[i])
+
+    while stack:
+        tokens.append(stack.pop())
+
+    for i in tokens:
+        if i in range(0, 10):
+            result.append(i)
+        else:
+            if i == "+":
+                p1 = result.pop()
+                p2 = result.pop()
+                result.append(p2 + p1)
+            elif i == "*":
+                p1 = result.pop()
+                p2 = result.pop()
+                result.append(p2 * p1)
+
+    print("#{} {}".format(test_case,result[-1]))
+
 for test_case in range(1,11):
     answer=0
     n=int(input())
@@ -5,12 +48,19 @@ for test_case in range(1,11):
     stack=[]
     temp=[]
     for i in arr:
+        if i==')':
+            chk=''
+            if chk=='(':
+                chk=stack.pop()
+                temp.append(chk)
+            temp.pop()
+            continue
         if i=='*':
-            while stack and stack[-1]=='*':
+            while stack and stack[-1]=='*' and stack[-1]!='(':
                 temp.append(stack.pop())
             stack.append(i)
         elif i=='+':
-            while stack:
+            while stack and stack[-1]!='(':
                 temp.append(stack.pop())
             stack.append(i)
         elif i.isdigit():
