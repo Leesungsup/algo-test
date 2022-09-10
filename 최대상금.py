ad61ef474d1ce2e7e57fa3236974447b9757248b
Leesungsup <tmdtjq0116@naver.com>
2022-09-11 01:40:39 +0900

    최대상금

--- 최대상금.py
+++ 최대상금.py
@@ -1,3 +1,29 @@
+T=int(input())
+def dfs(count):
+    global answer
+    if not count:
+        temp = int(''.join(values))
+        if answer < temp:
+            answer = temp
+        return
+    for i in range(length):
+        for j in range(i+1, length):
+            values[i], values[j] = values[j], values[i]
+            temp_key = ''.join(values)
+            if visited.get((temp_key, count-1), 1):
+                visited[(temp_key, count-1)] = 0
+                dfs(count-1)
+            values[i], values[j] = values[j], values[i]
+for test_case in range(1,T+1):
+    answer = 0
+    value, change = input().split()
+    values = list(value)
+    change = int(change)
+    length = len(values)
+    visited = {}
+    dfs(change)
+    print('#{} {}'.format(test_case, answer))
+
 def dfs(n,count):
     global result
     if count==int(cnt):
