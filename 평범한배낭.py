8aac92a877a3e6f722f7b423fc5fbe6aef7adda0
Leesungsup <tmdtjq0116@naver.com>
2022-11-14 03:19:43 +0900

    최대유량 애드몬드-카프 알고리즘

--- 평범한배낭.py
+++ 평범한배낭.py
@@ -3,13 +3,12 @@ m=[[0]*(k+1) for _ in range(n)]
 item=[]
 for i in range(n):
     item.append(list(map(int,input().split())))
-#냅색 문제 풀이
 for i in range(n):
-    for j in range(1, k + 1):
-        w = item[i][0]
-        v = item[i][1]
-        if j < w:
-            m[i][j] = m[i - 1][j]
+    for j in range(1,k+1):
+        w=item[i][0]
+        v=item[i][1]
+        if j>w:
+            m[i][j]=max(v+m[i-1][j-w],m[i-1][j])
         else:
-            m[i][j] = max(v + m[i - 1][j - w], m[i - 1][j])
+            m[i][j]=m[i-1][j]
 print(m[n-1][k])
