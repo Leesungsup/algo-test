b16b27b35d4ea495f824ca0dca4e8d0188a3517d
Leesungsup <tmdtjq0116@naver.com>
2023-01-15 23:58:27 +0900

    dp

---
+++ LCS.py
@@ -0,0 +1,11 @@
+a=input()
+b=input()
+lcs = [[0 for i in range(len(a)+1)] for j in range(len(b)+1)]
+for i in range(1, len(a)+1):
+    for j in range(1, len(b)+1):
+        if a[i-1] == b[j-1]:
+            lcs[i][j] = lcs[i-1][j-1] + 1
+        else:
+            lcs[i][j] = max(lcs[i][j-1], lcs[i-1][j])
+
+print(lcs[len(a)][len(b)])
