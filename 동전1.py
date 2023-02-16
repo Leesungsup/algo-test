b74f3b1648263a6b7546172387fc1f5de001b03d
Leesungsup <tmdtjq0116@naver.com>
2023-02-16 23:38:56 +0900

    동전1

--- 동전1.py
+++ 동전1.py
@@ -1,3 +1,5 @@
+import sys
+input=sys.stdin.readline
 n,k=map(int,input().split())
 arr=[]
 count=0
@@ -6,6 +8,6 @@ dp[0]=1
 for i in range(n):
     arr.append(int(input()))
 for i in range(n):
-    for j in range(arr[i],k):
-        dp[j]=dp[i]+dp[j-i]
+    for j in range(arr[i],k+1):
+        dp[j]=dp[j]+dp[j-arr[i]]
 print(dp[k])
