57597adc2a7b924295a44c77103f64fd370f66e3
Leesungsup <tmdtjq0116@naver.com>
2023-05-02 19:50:38 +0900

    05-02

--- 거스름돈.py
+++ 거스름돈.py
@@ -27,3 +27,47 @@ def solution1(n,money):
     print(dp)
     return answer
 solution1(5,[1,2,5])
+
+def solution2():
+    count=0
+    count1=0
+    answer=987654321
+    c=int(input())
+    n=c
+    count+=n//5
+    n=n-5*(n//5)
+    count+=n//2
+    n=n-2*(n//2)
+    if n==0:
+        answer=count
+    else:
+        k=1
+    n=c
+    count1+=n//2
+    n=n-2*(n//2)
+    if n==0:
+        if answer>count1:
+            answer=count1
+    print(answer)
+solution2()
+def sol(money):
+    cnt = 0
+
+    while money > 0:
+        if money % 5 == 0:
+            cnt += money//5
+            break
+        else:
+            money -= 2
+            cnt += 1
+
+    if money < 0:
+        print(-1)
+    else:
+        print(cnt)
+
+
+if __name__ == '__main__':
+    money = int(input())
+
+    sol(money)
